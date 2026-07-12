f7cb8ccacd808bb4f4c8fd877fd63a4489f1cb4c
Shanu Priya <priyads12@gmail.com>
2026-07-12 12:38:33 -0400

    GOOGLE places API - fetch tech groups in georgia

--- future-forge-AI-server/routes/opportunitiesRoute.js
+++ future-forge-AI-server/routes/opportunitiesRoute.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const router = express.Router();
 const { getOpportunities } = require("../services/opportunitiesService");
+const { getTechGroups } = require("../services/googlePlacesService");
 
-router.get("/opportunities",async (req, res)=>{
-   const result = await getOpportunities(req.query.role);
-   console.log(result);
+router.get("/opportunities", async (req, res) => {
+    const result = await getOpportunities(req.query.role);
+    console.log(result);
     res.json(result);
 });
+
+router.get("/techGroups", async (req, res) => {
+    try {
+        const result = await getTechGroups();
+        console.log(result);
+        res.json({
+            count: result.places?.length || 0,
+            places: result.places || []
+        });
+    } catch (error) {
+        res.status(500).json({
+            error: "Failed to fetch places",
+            details: error.message
+        });
+    }
+})
 
 module.exports = router;
